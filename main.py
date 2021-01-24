2eb892335b2523690efa78207909d17474039200
nathan saric <nathan@nathan.cgocable.net>
2021-01-23 23:59:49 -0500

    Database Work

--- main.py
+++ main.py
@@ -6,45 +6,65 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 db = SQLAlchemy(app)
 
+Achievement = db.Table('Achievement', db.Column('student_id', db.Integer, db.ForeignKey('student.id')), db.Column('course_id', db.Integer, db.ForeignKey('course.id')))
+
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    student_number = db.Column(db.Integer, nullable=False)
-    first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), nullable=False)
-    username = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(50), nullable=False)
-    program = db.Column(db.String(50), nullable=False)
-    year = db.Column(db.Integer, nullable=False)
-    image = db.Column(db.String(50), default='default.jpg')
-    achievements = db.relationship('Achievement', backref='student', lazy=True)
-    courses = db.relationship('Course', backref='student', lazy=True)
-
-class Achievement(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
-    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
-    grade = db.Column(db.Integer)
-    num_points = db.Column(db.Integer)
-    num_hours = db.Column(db.Integer)
+    student_number = db.Column(db.String(50))
+    first_name = db.Column(db.String(50))
+    last_name = db.Column(db.String(50))
+    password = db.Column(db.String(32))
+    achievements = db.relationship('Course', secondary='Achievement', backref=db.backref('student'))
 
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     course_name = db.Column(db.String(50))
-    image = db.Column(db.String(50), default='default.jpg')
-
-class Section(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
     instructor_id = db.Column(db.Integer, db.ForeignKey('instructor.id'))
-    section_number = db.Column(db.Integer)
-    semester = db.Column(db.String(50))
 
 class Instructor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(50))
     department = db.Column(db.String(50))
-    sections = db.relationship('Section', backref='instructor', lazy=True)
+    courses = db.relationship('Course', backref='instructor')
+
+### TO INITIALIZE NEW DATABASE ###
+# pipenv shell
+# python
+# from main import db
+# db.create_all()
+
+### TO INSERT DATA TO DATABASE ###
+# from main import *
+#
+### ADD A NEW STUDENT ###
+# <studentname> = Student(student_number='<data>', first_name='<data>', last_name='<data>', password='<data>')
+# db.session.add(<studentname>)
+# db.session.commit()
+#
+### ADD A NEW COURSE ###
+# <coursename> = Course(course_name='<data>', instructor=<instructorname>)
+# db.session.add(<coursename>)
+# db.session.commit()
+#
+### ADD A NEW INSTRUCTOR ###
+# <instructorname> = Instructor(first_name='<data>', last_name='<data>', department='<data>')
+# db.session.add(<instructorname>)
+# db.session.commit()
+#
+### ADD A STUDENT TO A COURSE ###
+# <coursename>.Achievement.append(<studentname>)
+# db.session.commit()
+
+### TO UPDATE DATA IN DATABASE ###
+# from main import *
+# update = <Class>.query.filter_by(id=<#>).first()
+# update.<attributename> = '<newdata>'
+# db.session.commit() 
+
+### TO DELETE DATA FROM DATABASE ###
+# db.session.delete(<attributename>)
+# db.session.commit()
 
 @app.route('/')
 @app.route('/index')
